ca4d627f3b8f185690fda563bab23db8387b0055
Quang PHAM <quangsoft@gmail.com>
2020-10-14 14:39:35 +0200

    Update IXMLSource.java

--- plugins/enablement/org.eclipse.datatools.enablement.oda.xml/src/org/eclipse/datatools/enablement/oda/xml/util/IXMLSource.java
+++ plugins/enablement/org.eclipse.datatools.enablement.oda.xml/src/org/eclipse/datatools/enablement/oda/xml/util/IXMLSource.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright (c) 2004, 2008 Actuate Corporation.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
  *
  * Contributors:
  *  Actuate Corporation  - initial API and implementation
@@ -20,4 +20,5 @@ public interface IXMLSource
 	InputStream openInputStream( ) throws OdaException;
 	String getEncoding( );
 	void release( ) throws OdaException;
+	int getSizeInM();
 }
